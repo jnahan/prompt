9e52722b4a1fae7dfe0d41c0f9749377d19ac0ac
jnahan <jenna5376@gmail.com>
2025-10-16 18:44:36 -0400

    updated navbar import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "./Navbar";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
